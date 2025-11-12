3902bddf87ded7baf5c168d978f7345f8bec85dd
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.local>
2025-11-12 13:33:42 +0000

    add procfile and foundation for heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qklf^m0e6!_=ym6fv092i=9!dnxt%9_7jkim+jeqfm99xm#+n*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
